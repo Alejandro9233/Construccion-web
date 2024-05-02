cd26c54c74c612d5ac85474a4ecefad5b13eda27
anapfigueroat <117108934+anapfigueroat@users.noreply.github.com>
2024-05-02 00:33:11 -0600

    ya acabamos gente :D

--- project/neoris-web/src/views/private/routes/crm/routes/game/index.jsx
+++ project/neoris-web/src/views/private/routes/crm/routes/game/index.jsx
@@ -1,7 +1,13 @@
 import React, {Fragment} from "react";
 import {Unity, useUnityContext} from "react-unity-webgl";
+import { ButtonStyled } from "./elements";
+import { useState } from "react";
 
 const Game = ({user}) => {
+  const [login, setLogin] = useState(false);
+  const [logout, setLogout] = useState(true);
+
+
   const { unityProvider, sendMessage } = useUnityContext({
     loaderUrl: "/codersTycoon1_1/build/codersTycoon1_1.loader.js",
     dataUrl: "/codersTycoon1_1/build/codersTycoon1_1.data",
@@ -12,25 +18,41 @@ const Game = ({user}) => {
   function logIn() {
     sendMessage("LogObject", "logIn", user?.id_usuario.toString());
     console.log(`logged in as player id: ${user?.id_usuario}`)
+    disableButtons();
+    updateButtonState(true, false);
   }
 
   function logOut() {
     sendMessage("LogObject", "logOut", user?.id_usuario.toString());
     console.log(`logged out as player id: ${user?.id_usuario}`)
+    disableButtons();
+    updateButtonState(false, true);
   }
 
-  return(
+  function updateButtonState(isLogin, isLogout) {
+    setTimeout(() => {
+      setLogin(isLogin);
+      setLogout(isLogout);
+    }, 3000);
+  }
   
-  <div>
-    <Fragment>
-        <Unity unityProvider={unityProvider} 
-        style={{height:560, width: 1000}}
-        />
-        <br />
-        <button onClick={logIn} >Log in</button>
-        <button onClick={logOut} >Log out</button>
-    </Fragment>
-  </div>
+  function disableButtons() {
+    setLogin(true);
+    setLogout(true);
+  }
+
+
+  return(
+      <Fragment>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <Unity unityProvider={unityProvider} style={{ height: 560, width: 1000 }} />
+          <br />
+          <div style={{display:'flex', flexDirection:'row', gap:'50px'}}>
+            <ButtonStyled type="primary" onClick={logIn} disabled={login}>Log in</ButtonStyled>
+            <ButtonStyled type="primary" onClick={logOut} disabled={logout}>Log out</ButtonStyled>
+          </div>
+        </div>
+      </Fragment>
   );
 };
 
